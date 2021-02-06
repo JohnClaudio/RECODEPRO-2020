5af5668d5d9af562e744b4c9eaf595b7438070d5
Joao Claudio <61328786+JohnClaudio@users.noreply.github.com>
2021-02-06 14:29:33 -0300

    Delete 7 - WeareaWeezer/src directory

--- 7 - WeareaWeezer/src/actions/musica.js
+++
@@ -1,8 +0,0 @@
-function letraMusica(letra){
-    return({
-    type: 'LETRA_MUSICA_ALTERADA', 
-    payload: letra 
-    })
-}
-
-export default letraMusica
--- 7 - WeareaWeezer/src/actions/tituloMusica.js
+++
@@ -1,8 +0,0 @@
-function tituloMusica(titulo){
-    return({
-    type: 'TITULO_MUSICA_ALTERADA', 
-    payload: titulo 
-    })
-}
-
-export default tituloMusica
